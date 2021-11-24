a0fcc6dda403e224a418b56dc14e4b18a2d0a28a
RazGavrieli <90526270+RazGavrieli@users.noreply.github.com>
2021-11-24 14:56:18 +0200

    added class lib. this class is for misc methods

---
+++ src/api/lib.java
@@ -0,0 +1,17 @@
+package api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class lib {
+
+    public static JSONObject parseJSONFile(String filename) throws JSONException, IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        return new JSONObject(content);
+    }
+
+}
